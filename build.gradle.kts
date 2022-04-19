f070f579c20619ea25d4d532a759a2b33fa489d7
a150238 <eivind.askestad@nav.no>
2022-04-19 16:41:47 +0200

    Added logstash-logback-encoder library
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,12 +15,14 @@ repositories {
 }
 
 dependencies {
-    implementation("ch.qos.logback:logback-classic")
+    //implementation("ch.qos.logback:logback-classic")
+    implementation("net.logstash.logback:logstash-logback-encoder:7.1.1")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.kafka:spring-kafka")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.kafka:spring-kafka-test")
 }
