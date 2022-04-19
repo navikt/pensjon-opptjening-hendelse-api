54a2fd74519cd4e52d070dcca75147b4d77a06b2
a150238 <eivind.askestad@nav.no>
2022-04-19 14:58:16 +0200

    Springoppsett

---
+++ src/main/kotlin/no/nav/pensjon/opptjening/pgiendring/PgiEndringApplication.kt
@@ -0,0 +1,11 @@
+package no.nav.pensjon.opptjening.pgiendring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PgiEndringApplication
+
+fun main(args: Array<String>) {
+    runApplication<PgiEndringApplication>(*args)
+}
---
+++ src/test/kotlin/no/nav/pensjon/opptjening/pgiendring/PgiEndringApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.nav.pensjon.opptjening.pgiendring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PgiEndringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
