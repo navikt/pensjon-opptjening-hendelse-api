de7adc50b0ccec1d0e5ece4a8406271f589caa0d
a150238 <eivind.askestad@nav.no>
2022-04-20 13:42:59 +0200

    Added pgi-endring topic and added KafkaConfig
    Co-authored-by: Mo Amini <mo.amini@nav.no>

---
+++ src/main/kotlin/no/nav/pensjon/opptjening/pgiendring/hendelse/config/KafkaConfig.kt
@@ -0,0 +1,44 @@
+package no.nav.pensjon.opptjening.pgiendring.hendelse.config
+
+import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.config.SslConfigs
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.ProducerFactory
+
+@EnableKafka
+@Configuration
+class KafkaConfig(
+    @param:Value("\${kafka.keystore.path}") private val keystorePath: String,
+    @param:Value("\${kafka.credstore.password}") private val credstorePassword: String,
+    @param:Value("\${kafka.truststore.path}") private val truststorePath: String,
+    @param:Value("\${kafka.brokers}") private val aivenBootstrapServers: String,
+    @param:Value("\${kafka.security.protocol}") private val securityProtocol: String,
+) {
+
+    @Bean
+    fun aivenProducerFactory(): ProducerFactory<String, String> = DefaultKafkaProducerFactory(aivenProducerConfig() + aivenSecurityConfig())
+
+    private fun aivenProducerConfig() = mapOf(
+        ProducerConfig.CLIENT_ID_CONFIG to "pgi-endring",
+        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to aivenBootstrapServers,
+    )
+
+    private fun aivenSecurityConfig() = mapOf(
+        SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG to keystorePath,
+        SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG to credstorePassword,
+        SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG to credstorePassword,
+        SslConfigs.SSL_KEY_PASSWORD_CONFIG to credstorePassword,
+        SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG to "JKS",
+        SslConfigs.SSL_KEYSTORE_TYPE_CONFIG to "PKCS12",
+        SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG to truststorePath,
+        CommonClientConfigs.SECURITY_PROTOCOL_CONFIG to securityProtocol
+    )
+}
