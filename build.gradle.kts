c385a79bc38237f255ff3e9ea38b4838145b9261
a150238 <eivind.askestad@nav.no>
2022-04-28 09:00:42 +0200

    Added variable for all dependency versions
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val springVersion = "2.6.7"
 val springKafkaVersion = "2.8.5"
-val navTokenSupportVersion = "2.0.14"
 val kotlinVersion = "1.6.21"
+val jacksonVersion = "2.13.2"
+val prometheusVersion = "1.8.5"
+val logbackEncoderVersion = "7.1.1"
+val navTokenSupportVersion = "2.0.14"
+val hibernateValidatorVersion = "7.0.4.Final"
 val mockWebserverVersion = "4.9.3"
 
 plugins {
@@ -29,21 +33,21 @@ dependencies {
     // Kotlin
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
 
     // Log and metric
-    implementation("io.micrometer:micrometer-registry-prometheus:1.8.5")
-    implementation("net.logstash.logback:logstash-logback-encoder:7.1.1")
+    implementation("io.micrometer:micrometer-registry-prometheus:$prometheusVersion")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logbackEncoderVersion")
 
     // OIDC
     implementation("no.nav.security:token-validation-spring:$navTokenSupportVersion")
     implementation("no.nav.security:token-client-spring:$navTokenSupportVersion")
-    implementation("org.hibernate:hibernate-validator:7.0.4.Final")
+    implementation("org.hibernate:hibernate-validator:$hibernateValidatorVersion")
 
-    //Test
+    // Test - setup
     testImplementation("org.springframework.boot:spring-boot-starter-test:$springVersion")
     testImplementation("org.springframework.kafka:spring-kafka-test:$springKafkaVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+    // Test - token-validation-spring-test dependencies
     testImplementation("no.nav.security:token-validation-spring-test:$navTokenSupportVersion")
     testImplementation("com.squareup.okhttp3:mockwebserver:$mockWebserverVersion")
     testImplementation("com.squareup.okhttp3:okhttp:$mockWebserverVersion")
