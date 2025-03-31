3b1a375ed036003e4070cd1cc2a9ccdff8b1d469
Arild Birkeland <arild.birkeland@nav.no>
2025-03-31 08:32:18 +0200

    Use secure logs (#5)
    Use secure logs.
    First check that onSuccess works as inteded, then change it from .info to .debug

--- src/main/kotlin/no/nav/pensjon/opptjening/hendelse/kafka/CustomProducerListener.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/hendelse/kafka/CustomProducerListener.kt
@@ -1,9 +1,8 @@
 package no.nav.pensjon.opptjening.hendelse.kafka
 
+import no.nav.pensjon.opptjening.hendelse.utils.PoppLogger
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.clients.producer.RecordMetadata
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.kafka.support.ProducerListener
 import org.springframework.lang.Nullable
 import org.springframework.stereotype.Component
@@ -12,7 +11,9 @@ import java.time.LocalDateTime
 @Component
 class CustomProducerListener : ProducerListener<String, String> {
 
-    private val log: Logger = LoggerFactory.getLogger(this::class.java)
+    companion object {
+        private val log = PoppLogger(this::class.java)
+    }
 
     init {
         log.info("Custom producer listener initialized")
@@ -20,12 +21,12 @@ class CustomProducerListener : ProducerListener<String, String> {
 
     override fun onSuccess(producerRecord: ProducerRecord<String, String>?, recordMetadata: RecordMetadata?) {
         log.info(
-            "Kafka message published successfully - Topic: {}, Partition: {}, Offset: {}, Timestamp: {}, Key: {}",
-            producerRecord?.topic() ?: "unknown",
-            producerRecord?.partition() ?: -1,
-            recordMetadata?.offset() ?: -1,
-            LocalDateTime.now(),
-            producerRecord?.key() ?: "null"
+            "Kafka message published successfully - " +
+                    "Topic: ${producerRecord?.topic() ?: "unknown"}, " +
+                    "Partition: ${producerRecord?.partition() ?: "unknown"}, " +
+                    "Offset: ${recordMetadata?.offset() ?: "unknown"}, " +
+                    "Timestamp: ${LocalDateTime.now()}, " +
+                    "Key: ${producerRecord?.key() ?: "unknown"}"
         )
     }
 
@@ -35,12 +36,14 @@ class CustomProducerListener : ProducerListener<String, String> {
         exception: Exception?
     ) {
         log.error(
-            "Kafka message publishing failed - Topic: {}, Partition: {}, Key: {}",
-            producerRecord?.topic() ?: "unknown",
-            producerRecord?.partition() ?: -1,
-            producerRecord?.key() ?: "null",
-            exception
+            "Kafka message publishing failed - " +
+                    "Topic: ${producerRecord?.topic() ?: "unknown"}, " +
+                    "Partition: ${producerRecord?.partition() ?: "unknown"}, " +
+                    "Offset: ${recordMetadata?.offset() ?: "unknown"}, " +
+                    "Timestamp: ${LocalDateTime.now()}, " +
+                    "Key: ${producerRecord?.key() ?: "unknown"}" +
+                    "Payload: ${producerRecord?.value() ?: "unknown"}" +
+                    "Exception: ${exception?.message ?: "unknown"}"
         )
-        log.error("Failed message payload: {}", producerRecord?.value())
     }
 }
---
+++ src/main/kotlin/no/nav/pensjon/opptjening/hendelse/utils/PoppLogger.kt
@@ -0,0 +1,57 @@
+package no.nav.pensjon.opptjening.hendelse.utils
+
+import net.logstash.logback.marker.LogstashMarker
+import net.logstash.logback.marker.Markers
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+
+class PoppLogger(clazz: Class<*>) {
+
+    companion object {
+        const val PERSON_ID_MARKER = "personId"
+        const val EXCEPTION_TYPE_MARKER = "exceptionType"
+    }
+
+    private val log = LoggerFactory.getLogger(clazz)
+
+    fun debug(msg: String, ex: Throwable? = null, markers: MarkerProperties? = null) {
+        val marker = toMarker(markers, ex)
+        log.debug(marker, maskFnr(msg))
+        SecureLog.debug(marker, msg, ex)
+    }
+
+    fun info(msg: String, ex: Throwable? = null, markers: MarkerProperties? = null) {
+        val marker = toMarker(markers, ex)
+        log.info(marker, maskFnr(msg))
+        SecureLog.info(marker, msg, ex)
+    }
+
+    fun warn(msg: String, ex: Throwable? = null, markers: MarkerProperties? = null) {
+        val marker = toMarker(markers, ex)
+        log.warn(marker, maskFnr(msg))
+        SecureLog.warn(marker, msg, ex)
+    }
+
+    fun error(msg: String, ex: Throwable? = null, markers: MarkerProperties? = null) {
+        val marker = toMarker(markers, ex)
+        log.error(marker, maskFnr(msg))
+        SecureLog.error(marker, msg, ex)
+    }
+
+    private fun toMarker(markers: Map<String, Any?>?, ex: Throwable?): LogstashMarker {
+        return markers?.let {
+            Markers.appendEntries(markers.plus(EXCEPTION_TYPE_MARKER to (ex?.let { ex -> ex::class.qualifiedName }
+                ?: "")))
+        } ?: Markers.empty()
+    }
+
+    class MarkerProperties(properties: Map<String, Any?>) : Map<String, Any?> by properties
+}
+
+private val logger: Logger = LoggerFactory.getLogger("secure")
+
+object SecureLog : Logger by logger
+
+private val fnrRegex = "(\\d{6})\\d{5}".toRegex()
+fun maskFnr(text: String?) = text?.let { fnrRegex.replace(it, "\$1*****") }
