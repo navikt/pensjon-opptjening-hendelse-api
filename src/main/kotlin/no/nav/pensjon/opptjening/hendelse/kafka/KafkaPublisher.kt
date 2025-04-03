6e4b4b670638df9cb93bdce8880b75722c927233
Arild Birkeland <arild.birkeland@nav.no>
2025-04-03 07:21:10 +0200

    chore(): log Offsets received

--- src/main/kotlin/no/nav/pensjon/opptjening/hendelse/kafka/KafkaPublisher.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/hendelse/kafka/KafkaPublisher.kt
@@ -1,5 +1,7 @@
 package no.nav.pensjon.opptjening.hendelse.kafka
 
+import no.nav.pensjon.opptjening.hendelse.api.HendelseService
+import no.nav.pensjon.opptjening.hendelse.utils.PoppLogger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
@@ -16,22 +18,26 @@ class KafkaPublisher(
     @Value("\${OPPTJENING_ENDRET_TOPIC}") private val opptjeningEndretTopic: String
 ) : Publisher {
 
+    companion object {
+        private val log = PoppLogger(HendelseService::class.java)
+    }
+
     init {
         kafkaTemplate.setProducerListener(customProducerListener)
     }
 
     override fun publish(hendelser: List<Pair<EndringsType, String>>): List<Long> {
         return kafkaTemplate.executeInTransaction { template ->
-            hendelser
-                .map { (type, hendelse) ->
-                    when (type) {
-                        EndringsType.ENDRET_BEHOLDNING -> template.send(beholdningEndretTopic, hendelse)
-                        EndringsType.ENDRET_OPPTJENING -> template.send(opptjeningEndretTopic, hendelse)
-                    }
+            val futures = hendelser.map { (type, hendelse) ->
+                when (type) {
+                    EndringsType.ENDRET_BEHOLDNING -> template.send(beholdningEndretTopic, hendelse)
+                    EndringsType.ENDRET_OPPTJENING -> template.send(opptjeningEndretTopic, hendelse)
                 }
-                .map {
-                    it.get().recordMetadata.offset()
-                }
+            }
+
+            val offsets = futures.map { it.get().recordMetadata.offset() }
+            log.info("Total messages sent: ${futures.size}, Offsets received: $offsets")
+            offsets
         }
     }
 }
