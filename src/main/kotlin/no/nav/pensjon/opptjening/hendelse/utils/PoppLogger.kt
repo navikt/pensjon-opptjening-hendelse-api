84a22ddc162437e54f2f9a0e730eb4161bf73e63
Arild Birkeland <arild.birkeland@nav.no>
2025-04-04 09:00:45 +0200

    chore(): try to add back secure logger

--- src/main/kotlin/no/nav/pensjon/opptjening/hendelse/utils/PoppLogger.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/hendelse/utils/PoppLogger.kt
@@ -18,25 +18,25 @@ class PoppLogger(clazz: Class<*>) {
     fun debug(msg: String, ex: Throwable? = null, markers: MarkerProperties? = null) {
         val marker = toMarker(markers, ex)
         log.debug(marker, maskFnr(msg))
-//        SecureLog.debug(marker, msg, ex)
+        SecureLog.debug(marker, msg, ex)
     }
 
     fun info(msg: String, ex: Throwable? = null, markers: MarkerProperties? = null) {
         val marker = toMarker(markers, ex)
         log.info(marker, maskFnr(msg))
-//        SecureLog.info(marker, msg, ex)
+        SecureLog.info(marker, msg, ex)
     }
 
     fun warn(msg: String, ex: Throwable? = null, markers: MarkerProperties? = null) {
         val marker = toMarker(markers, ex)
         log.warn(marker, maskFnr(msg))
-//        SecureLog.warn(marker, msg, ex)
+        SecureLog.warn(marker, msg, ex)
     }
 
     fun error(msg: String, ex: Throwable? = null, markers: MarkerProperties? = null) {
         val marker = toMarker(markers, ex)
         log.error(marker, maskFnr(msg))
-//        SecureLog.error(marker, msg, ex)
+        SecureLog.error(marker, msg, ex)
     }
 
     private fun toMarker(markers: Map<String, Any?>?, ex: Throwable?): LogstashMarker {
@@ -49,9 +49,9 @@ class PoppLogger(clazz: Class<*>) {
     class MarkerProperties(properties: Map<String, Any?>) : Map<String, Any?> by properties
 }
 
-//private val logger: Logger = LoggerFactory.getLogger("secure")
+private val logger: Logger = LoggerFactory.getLogger("secure")
 
-//object SecureLog : Logger by logger
+object SecureLog : Logger by logger
 
 private val fnrRegex = "(\\d{6})\\d{5}".toRegex()
 fun maskFnr(text: String?) = text?.let { fnrRegex.replace(it, "\$1*****") }
