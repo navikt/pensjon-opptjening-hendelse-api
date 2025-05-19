e454a25a5faef8a42e03a946b926413fef4d3f8b
Arild Birkeland <arild.birkeland@nav.no>
2025-05-19 13:10:46 +0200

    feat(): add team logs (#15)

--- src/main/kotlin/no/nav/pensjon/opptjening/hendelse/utils/PoppLogger.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/hendelse/utils/PoppLogger.kt
@@ -1,45 +1,33 @@
 package no.nav.pensjon.opptjening.hendelse.utils
 
-import net.logstash.logback.marker.LogstashMarker
-import net.logstash.logback.marker.Markers
 import org.slf4j.LoggerFactory
-
+import org.slf4j.MarkerFactory
 
 class PoppLogger(clazz: Class<*>) {
 
-    companion object {
-        const val PERSON_ID_MARKER = "personId"
-        const val EXCEPTION_TYPE_MARKER = "exceptionType"
-    }
-
     private val log = LoggerFactory.getLogger(clazz)
+    private val teamLogsMarker = MarkerFactory.getMarker("TEAM_LOGS")
 
-    fun debug(msg: String, ex: Throwable? = null, markers: MarkerProperties? = null) {
-        val marker = toMarker(markers, ex)
-        log.debug(marker, msg, ex)
+    fun debug(msg: String, ex: Throwable? = null) {
+        log.debug(maskFnr(msg))
+        log.debug(teamLogsMarker, msg, ex)
     }
 
-    fun info(msg: String, ex: Throwable? = null, markers: MarkerProperties? = null) {
-        val marker = toMarker(markers, ex)
-        log.info(marker, msg, ex)
+    fun info(msg: String, ex: Throwable? = null) {
+        log.info(maskFnr(msg))
+        log.info(teamLogsMarker, msg, ex)
     }
 
-    fun warn(msg: String, ex: Throwable? = null, markers: MarkerProperties? = null) {
-        val marker = toMarker(markers, ex)
-        log.warn(marker, msg, ex)
+    fun warn(msg: String, ex: Throwable? = null) {
+        log.warn(maskFnr(msg))
+        log.warn(teamLogsMarker, msg, ex)
     }
 
-    fun error(msg: String, ex: Throwable? = null, markers: MarkerProperties? = null) {
-        val marker = toMarker(markers, ex)
-        log.error(marker, msg, ex)
+    fun error(msg: String, ex: Throwable? = null) {
+        log.error(maskFnr(msg))
+        log.error(teamLogsMarker, msg, ex)
     }
 
-    private fun toMarker(markers: Map<String, Any?>?, ex: Throwable?): LogstashMarker {
-        return markers?.let {
-            Markers.appendEntries(markers.plus(EXCEPTION_TYPE_MARKER to (ex?.let { ex -> ex::class.qualifiedName }
-                ?: "")))
-        } ?: Markers.empty()
-    }
-
-    class MarkerProperties(properties: Map<String, Any?>) : Map<String, Any?> by properties
+    private val fnrRegex = "(\\d{6})\\d{5}".toRegex()
+    private fun maskFnr(text: String?) = text?.let { fnrRegex.replace(it, "\$1*****") }
 }
